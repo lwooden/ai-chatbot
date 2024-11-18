4e9943d861c659ac782da74902732ca4fc40779a
lwooden <lowell.wooden@gmail.com>
2024-11-18 17:03:02 -0500

    initial commit from local dev

---
+++ lib/ai/index.ts
@@ -0,0 +1,35 @@
+import { openai } from "@ai-sdk/openai"
+import { ollama } from "ollama-ai-provider"
+import { google } from "@ai-sdk/google"
+import {
+  experimental_wrapLanguageModel as wrapLanguageModel,
+  streamText,
+} from "ai"
+import { OpenAI as oa } from "openai"
+
+import { customMiddleware } from "./custom-middleware"
+
+// Adding OpenAI models
+export const customModel = (apiIdentifier: string) => {
+  return wrapLanguageModel({
+    model: openai(apiIdentifier),
+    middleware: customMiddleware,
+  })
+}
+// Adding Gemini 1.5 model
+export const customModel2 = (apiIdentifier: string) => {
+  return wrapLanguageModel({
+    model: google(apiIdentifier),
+    middleware: customMiddleware,
+  })
+}
+
+// Adding Ollama model
+// export const customModel3 = ollama({
+//   baseURL: "http://localhost:11434/api",
+// })
+
+// export const localOllama = new oa({
+//   baseURL: "http://localhost:11434/v1",
+//   apiKey: "ollama", // required but unused
+// })
